0e0e2e2b3084b8a6f406a4da18dd72864755724c
Harsharajapanthula <harsharajapanthula@gmail.com>
2026-06-15 18:47:21 +0530

    Initial commit

---
+++ mysite/myapp/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render,redirect
+
+from .forms import ExpenseForm
+from .models import Expense
+from django.db.models import Sum
+import datetime
+
+# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        expense = ExpenseForm(request.POST)
+        if expense.is_valid():
+            expense.save()
+
+    expenses = Expense.objects.all()
+    total_expenses = expenses.aggregate(Sum("amount"))
+    expense_form = ExpenseForm()
+    last_year = datetime.date.today() - datetime.timedelta(days=365)
+    data = Expense.objects.filter(date__gt = last_year)
+    yearly_sum = data.aggregate(Sum("amount"))
+
+    last_month = datetime.date.today() - datetime.timedelta(days=30)
+    data = Expense.objects.filter(date__gt = last_month)
+    monthly_sum = data.aggregate(Sum("amount"))
+
+    last_weekly = datetime.date.today() - datetime.timedelta(days=7)
+    data = Expense.objects.filter(date__gt = last_weekly)
+    weekly_sum = data.aggregate(Sum("amount"))
+
+    dailysums = Expense.objects.filter().values('date').order_by('date').annotate(sum = Sum('amount'))
+
+    category_sums = Expense.objects.filter().values('category').order_by('category').annotate(sum = Sum('amount'))
+
+
+
+
+    return render(request, 'myapp/dark-modern-index.html', {'expense_form': expense_form, 'expenses': expenses, 'total_expenses': total_expenses, 'yearly_sum': yearly_sum, 'monthly_sum': monthly_sum, 'weekly_sum': weekly_sum   , 'daily_sums': dailysums, 'categorical_sums': category_sums})
+
+
+def edit(request, id):
+    expense = Expense.objects.get(id=id)
+    expense_form = ExpenseForm(instance=expense)
+    if request.method == 'POST':
+        expense_form = ExpenseForm(request.POST, instance=expense)
+        if expense_form.is_valid():
+            expense_form.save()
+            return redirect('index')
+    return render(request, 'myapp/edit.html', {'expense_form': expense_form})
+
+def delete(request, id):
+    if request.method == 'POST':
+        expense = Expense.objects.get(id=id)
+        expense.delete()
+    return redirect('index')
